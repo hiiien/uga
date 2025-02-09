f7907b167dbfaa015b7abf5ccecfba557552a17e
ChristianHirschey <christian.hirschey@gmail.com>
2025-02-09 00:52:53 +0000

    auth0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
 import { redirect } from "next/navigation";
+import Image from "next/image";
+import { auth0 } from "@/lib/auth0";
+import { daysInWeek } from "date-fns/constants";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">
+          <button>Sign up</button>
+        </a>
+        <a href="/auth/login">
+          <button>Log in</button>
+        </a>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+      <p>
+        <a href="/auth/logout">
+          <button>Log out</button>
+        </a>
+      </p>
+    </main>
+  )
+  
   redirect("/dashboard");
 }
