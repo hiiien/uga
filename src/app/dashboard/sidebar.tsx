e7d292f17f49184bc7040b95c48c014a651af60d
ChristianHirschey <christian.hirschey@gmail.com>
2025-02-09 07:30:47 +0000

    add dashboard reroute

--- src/app/dashboard/sidebar.tsx
+++ src/app/dashboard/sidebar.tsx
@@ -2,8 +2,9 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { Calendar, Settings, HelpCircle } from "lucide-react";
+import { Calendar, Settings, HelpCircle} from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { LineChartIcon } from "lucide-react";
 
 export function Sidebar() {
   const router = useRouter();
@@ -20,6 +21,13 @@ export function Sidebar() {
         >
           <Calendar className="h-5 w-5" />
           Message Scheduler
+        </Button>
+        <Button
+          onClick={() => router.push("/dashboard/")}
+          className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 hover:bg-teal-100/30 hover:text-gray-900 w-full"
+        >
+          <LineChartIcon className="h-5 w-5" />
+          Dashboard
         </Button>
       </nav>
       <div className="mt-auto pt-8">
